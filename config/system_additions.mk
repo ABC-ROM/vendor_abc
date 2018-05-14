ABCrom-Kenzo/abc-n_vendor_nexus

prebuilt: Add apn-conf.xml

configs/system_additions.mk
@@ -21,6 +21,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/nexus/prebuilt/etc/init.abc.rc:root/init.abc.rc
 
+# APN
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/nexus/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \

ABC-ROM/vendor_abc

config/system_additions.mk
@@ -25,6 +25,10 @@ ifneq (true,$(PRODUCT_FULL_TREBLE))
     vendor/aosp/prebuilt/etc/init.abc.rc:root/init.abc.rc
 endif
 
+# APN
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \